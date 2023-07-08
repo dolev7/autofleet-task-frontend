deb4e90f90b8e1eee635e4cbe808d4f32ad4d67e
Daniel <68538339+dolev7@users.noreply.github.com>
2023-07-09 00:58:36 +0300

    moved Map component out of the App component

--- src/components/Map/index.tsx
+++ src/components/Map/index.tsx
@@ -1,20 +1,86 @@
-export {}
-// import { useMapEvents , FeatureGroup} from 'react-leaflet';
-// import { EditControl } from 'react-leaflet-draw';
+import { useState, useEffect } from 'react';
+import { Polygon, OverlayView, GoogleMap, useJsApiLoader} from '@react-google-maps/api';
 
 
-// export const DrawEvents = ({ onPolygonDrawn }: any) => {
-//   const handleDrawCreated = (event: any) => {
-//     const { layerType, layer } = event;
-//     if (layerType === 'polygon') {
-//       const polygon = layer.getLatLngs()[0];
-//       onPolygonDrawn(polygon);
-//     }
-//   };
+const Map = ({polygonCoordinates, setPolygonCoordinates, vehiclesLocations}: any) => {
+  const [showPolygon, setShowPolygon] = useState(false);
+  const [showVehicles, setShowVehicles] = useState(false);
 
-//   return (
-//     <FeatureGroup>
-//       <EditControl position="topright" draw={{ rectangle: false }} onCreated={handleDrawCreated} />
-//     </FeatureGroup>
-//   );
-// };
+  useEffect(() => {
+    setShowPolygon(true);
+  }, [polygonCoordinates]);
+
+  useEffect(() => {
+    setShowVehicles(true);
+  }, [vehiclesLocations]);
+
+  const handleMapClick = (event : any) => {
+    const lat = event.latLng.lat();
+    const lng = event.latLng.lng();
+    if (showVehicles) {
+      setShowVehicles(false);
+      setPolygonCoordinates([]);
+    }
+    //@ts-ignore
+    setPolygonCoordinates((prevCoordinates) => [...prevCoordinates, { lat, lng }]);
+  };
+
+  const CarOverlay = () => (
+    <div style={{ position: 'absolute', transform: 'translate(-50%, -50%)' }}>
+      <img src="https://www.svgrepo.com/download/10703/car.svg" alt="Car Icon" width="40" height="40" />
+    </div>
+  );
+
+
+  const mapContainerStyle = {
+    width: '100%',
+    height: '400px',
+  };
+
+  const { isLoaded } = useJsApiLoader({
+    googleMapsApiKey: 'AIzaSyBQgYDGbw-UjuTy8BMapIcvUUkKVTHw9P8',
+  });
+
+  if (!isLoaded) {
+    return <div>Loading...</div>;
+  }
+
+
+const options = {
+  fillColor: 'lightblue',
+  fillOpacity: 0.6,
+  strokeColor: 'red',
+  strokeOpacity: 1,
+  strokeWeight: 2,
+};
+
+  return (
+    <div style={{ height: '400px', width: '100%' }}>
+      <GoogleMap
+        mapContainerStyle={mapContainerStyle}
+        center={{ lat: 51.505, lng: -0.09 }}
+        zoom={13}
+        onClick={handleMapClick}
+      >
+        {showVehicles && 
+        vehiclesLocations.map((vehicle : any, index : any) => (
+        <OverlayView
+          key={index}
+          position={vehicle.location}
+          mapPaneName={OverlayView.OVERLAY_MOUSE_TARGET}
+          >
+          <CarOverlay />
+        </OverlayView>
+      ))}
+      {showPolygon && 
+      <Polygon
+          paths={polygonCoordinates}
+          options={options}
+      />
+  }
+      </GoogleMap>
+    </div>
+  );
+};
+
+export default Map;
--- src/network/index.tsx
+++ src/network/index.tsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const BACKEND_URL = process.env.REACT_APP_BACKEND_URL;
-export const network = axios.create({ baseURL: BACKEND_URL });
+const network = axios.create({ baseURL: BACKEND_URL });
 
 
 export const fetchVehiclesLocations = async () => {
